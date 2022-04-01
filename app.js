e043cd90f4691ad2d28a6db56988986bb833fda2
Eric Spencer <spencer22eric@gmail.com>
2022-03-31 21:34:31 -0400

    generate HTML file

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
+const pageHTML = generatePage(name, github);
 
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('index.html', pageHTML, err => {
+  if (err) throw err;
 
-    console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
