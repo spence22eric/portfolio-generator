6d8b0493506471ac9913e91d558b657864160afa
Eric Spencer <spencer22eric@gmail.com>
2022-04-02 11:45:05 -0400

    add user input with inquirer npm

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', pageHTML, err => {
-  if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        /* Pass your questions in here */
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then((answers) => {
+        console.log(answers);
+    });
+    // .catch((error) => {
+    //     if (error.isTtyError) {
+    //         // Prompt couldn't be rendered in the current environment
+    //     } else {
+    //         // Something else went wrong
+    //     }
+    // });
